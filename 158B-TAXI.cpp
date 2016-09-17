c0b7faecd47bec626a08ffdf6bb3f05b37e9ec19
Himanshu Dixit <hudixt@gmail.com>
2016-09-17 12:25:24 +0530

    Update 158B-TAXI.cpp
    Fixed For Test Case #90, removed other output for OJ

--- 158B-TAXI.cpp
+++ 158B-TAXI.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <stdlib.h>
+
 using namespace std;
 
 int main(){
 int group_total,i,k;
-cout<<"Enter Total No Of Groups";
-cin>>group_total;
-int *student_group = (int *) malloc(sizeof(int)*group_total);
 
+cin>>group_total;
+//int* student_group = (int *) malloc(sizeof(int)*group_total); Use if want to DMAelse
+int student_group[5]={0}; //intializing all elements with zero
 for(i=0;i<group_total;i++){
 cin>>k;
 student_group[k]++;
@@ -15,15 +16,24 @@ student_group[k]++;
 int taxi=0;
 taxi+=student_group[4];
 taxi+=student_group[3];
-student_group[1]=student_group[1]-student_group[3];student_group[1]=student_group[1]<0?0:student_group[1];
+student_group[1]=student_group[1]-student_group[3];
+student_group[1]=student_group[1]<0?0:student_group[1];
 if(student_group[1]<0){
 student_group[1]=0;
 }
-taxi+=student_group[2]/2;
+
+taxi+=student_group[2]/2+student_group[2]%2;
+
 student_group[2]=student_group[2]%2;
-taxi+=(student_group[2]+student_group[1])/4;
-taxi+=(student_group[2]+student_group[1])%4;
-cout<<taxi;
-free(student_group);
+
+student_group[1] = student_group[1]-2*(student_group[2]);
+if(student_group[1]>0){
+taxi+=(student_group[1])/4;
+if(student_group[1]%4>0){
+taxi++;
+}
+}
+
+cout<<taxi; //Output the no of taxi neesded by the group
 return 0;
 }
